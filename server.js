6adbd2cc71f0a03d71be67df43f8b2620d5d2251
Jake Kaplan <kaplanjt@gmail.com>
2021-10-11 15:54:43 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+
+//Dependencies
+const express = require('express');
+const methodOverride = require('method-override');
+const mongoose = require ('mongoose');
+const app = express();
+const db = mongoose.connection;
+
+//PORT
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3000;
+
+//DATABASE
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is mongodb not running?'));
+db.on('connected', () => console.log('mongodb connected'));
+db.on('disconnected', () => console.log('mongodb disconnected'));
+
+
+//MIDDLEWARE
+//parse
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"))
+
+
+//use public folder for static assets
+app.use(express.static('public'));
+
+// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+
+//use method override
+app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+
+
+// ROUTES
+app.get('/' , (req, res) => {
+  res.send('Yay! the app works');
+});
+
+//LISTENER
+app.listen(3000, () => console.log('listening'));
